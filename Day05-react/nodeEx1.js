fb6beb8925d7eb3fa8cdd993fba415f633ebdc5d
shubhamAW <waykarshubham2022@gmail.com>
2023-02-17 10:26:22 +0530

    day05

---
+++ Day05-react/nodeEx1.js
@@ -0,0 +1,8 @@
+// Basic add function in node.js
+function add(a,b){
+    return a+b;
+}
+
+let a=2,b=4;
+console.log("hello everyone");
+console.log(add(a,b));
---
+++ Day05-react/nodeEx2.js
@@ -0,0 +1,6 @@
+function add(x,y){
+    return x+y;
+}
+function sub(x,y){
+    return x-y;
+}
